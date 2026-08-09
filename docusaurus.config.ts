46c419d57200453b90f3ed7a2041012c26ef7595
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2026-08-09 14:28:28 -0300

    Done

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,142 +1,132 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Motorola G24/G24 Power',
+  title: "Motorola G24/G24 Power",
   // tagline: 'Documentation',
   // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://fogorow.fuckyoumoto.xyz',
+  url: "https://fuckyoumotorola.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/fogorow-documentation/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'moto-fogorow', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+  organizationName: "fuckyoumotorola", // Usually your GitHub org/user name.
+  projectName: "fogorow-documentation", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/moto-fogorow/documentation/edit/main/',
+            "https://github.com/fuckyoumotorola/fogorow-documentation/edit/main/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
-  plugins: [
-    'docusaurus-plugin-zooming',
-    'docusaurus-lunr-search'
-  ],
+  plugins: ["docusaurus-plugin-zooming", "docusaurus-lunr-search"],
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     metadata: [
-      // {name: 'google-site-verification', content: '<KEY>'},
     ],
     announcementBar: {
-      id: 'bootloader_unlock',
+      id: "bootloader_unlock",
       content:
-        '🎉 Found a way to unlock the bootloader for free with mtkclient!',
-      backgroundColor: '#fafbfc',
-      textColor: '#091E42',
+        "🎉 Found a way to unlock the bootloader for free with mtkclient!",
+      backgroundColor: "#fafbfc",
+      textColor: "#091E42",
       isCloseable: true,
     },
     navbar: {
-      title: 'Motorola G24/G24 Power',
-      // logo: {
-      //   alt: 'My Site Logo',
-      //   src: 'img/logo.svg',
-      // },
+      title: "Motorola G24/G24 Power",
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Documentation',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Documentation",
         },
         {
-          href: 'https://fuckyoumoto.xyz',
-          position: 'left',
-          label: 'Fuckyoumoto project',
+          href: "https://fuckyoumoto.xyz",
+          position: "left",
+          label: "Fuckyoumoto project",
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/moto-fogorow/documentation',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/fuckyoumotorola/fogorow-documentation",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Content',
+          title: "Content",
           items: [
             {
-              label: 'Documentation',
-              to: '/docs/about',
+              label: "Documentation",
+              to: "/docs/about",
             },
           ],
         },
         {
-          title: 'Get in touch',
+          title: "Get in touch",
           items: [
             {
-              label: 'Forum',
-              href: 'https://github.com/orgs/moto-fogorow/discussions',
+              label: "Forum",
+              href: "https://github.com/orgs/moto-fogorow/discussions",
             },
             {
-              label: 'Telegram',
-              href: 'https://t.me/motoheliog85',
+              label: "Telegram",
+              href: "https://t.me/motoheliog85",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/orgs/moto-fogorow/',
+              label: "GitHub",
+              href: "https://github.com/orgs/moto-fogorow/",
             },
           ],
         },
@@ -144,15 +134,15 @@ const config: Config = {
       copyright: `Copyright © ${new Date().getFullYear()} fuckyoumoto`,
     },
     zooming: {
-      selector: '.markdown img',
+      selector: ".markdown img",
       delay: 500,
       background: {
-        light: 'rgba(101,108,133,0.8)',
-        dark: 'rgba(9,10,17,0.8)'
+        light: "rgba(101,108,133,0.8)",
+        dark: "rgba(9,10,17,0.8)",
       },
       options: {
         // See the docs of zooming for all available options: https://github.com/francoischalifour/medium-zoom#usage
-      }
+      },
     },
     prism: {
       theme: prismThemes.github,
